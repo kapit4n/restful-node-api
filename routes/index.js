298af45f5d670ee9e31c2c93510f35fec2321f47
Luis Arce <luis.arce22@gmail.com>
2019-04-05 10:48:58 -0400

    Updated endpoints

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/data', function(req, res, next) {
+  res.send('This is all data');
+});
+
+router.get('/data/:id', function(req, res, next) {
+  res.send(req.params);
+});
+
+router.post('/data', function(req, res, next) {
+  res.send('Post example');
+});
+
+router.put('/data/:id', function(req, res, next) {
+  res.send('Put info');
+});
+
 module.exports = router;
