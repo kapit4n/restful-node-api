b4ade27c17c5e9295778971489a55007fea8290b
Luis Arce <luis.arce22@gmail.com>
2019-04-05 12:00:32 -0400

    Generate paginated data

--- app.js
+++ app.js
@@ -6,9 +6,31 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let port = 3001;
 
 var app = express();
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  //res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+//app.use(cors(corsOptions))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,29 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/data', function (req, res, next) {
-  res.send([{
-    id: 1,
-    name: "Customer 1",
-    code: 123,
-    address: "Bolivia, Cochabamba, America St 1212",
-    budget: 100
-  }, {
-    id: 2,
-    name: "Customer 2",
-    code: 123,
-    address: "Bolivia, Cochabamba, America St 1212",
-    budget: 100
-  }]);
+router.get('/data/', function (req, res, next) {
+
+  let top = req.query.$top ? req.query.$top: 10;
+  let skip = req.query.$skip ? req.query.$skip : 0;
+
+  console.log(top);
+  console.log(skip);
+  let customers = [];
+  let count = 1000;
+  for (let i = 0; i < top; i++) {
+    customers.push({
+      id: i + skip,
+      name: "Customer " + i + skip,
+      code: 123,
+      address: "Bolivia, Cochabamba, America St " + i + skip,
+      budget: 10 * i
+    });
+  }
+
+  res.send({
+    count: count,
+    value: customers
+  });
 });
 
 router.get('/data/:id', function (req, res, next) {
