80ea0754bd0f20a76f08dec0cd1814f3902ab04a
Luis Arce <luis.arce22@gmail.com>
2019-04-16 15:53:06 -0400

    Adding sort by and paging

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,38 @@ router.get('/', function (req, res, next) {
   });
 });
 
+var allData = [];
+var allDataCount = 1000;
+for (let i = 0; i < allDataCount; i++) {
+  let nextNumber = i  % 10;
+  let nextCodeNumber = i  % 7;
+  let nextAddressNumber = i  % 8;
+  let nextValue = {
+    id: i,
+    name: "Customer " + nextNumber,
+    code: "cd - " + (100 + nextCodeNumber ),
+    address: "Bolivia, Am St " + nextAddressNumber,
+    budget: 10 * i
+  };
+  allData.push(nextValue);
+}
+
+var filterValues = {};
+filterValues['code'] = [...Array(7).keys()].map(x => "cd - " + (100 + x) );
+filterValues['address'] = [...Array(8).keys()].map(x => "Bolivia, Am St " + x );
+filterValues['name'] = [...Array(10).keys()].map(x => "Customer " + (x) );
+
 router.get('/data/', function (req, res, next) {
 
-  let top = req.query.$top ? req.query.$top: 10;
-  let skip = req.query.$skip ? req.query.$skip : 0;
+  let top = req.query.$top ? parseInt(req.query.$top): 10;
+  let skip = req.query.$skip ? parseInt(req.query.$skip) : 0;
 
   let filter = req.query.$filter;
 
   let filterArr = {};
   if (filter) {
-    filter = filter.replace('[', '');
-    filter = filter.replace(']', '');
+    filter = filter.replace(/\[/g, "");
+    filter = filter.replace(/\]/g, "");
     filter = decodeURIComponent(filter);
     let conditions = filter.split('AND');
     conditions.forEach(element => {
@@ -35,43 +56,56 @@ router.get('/data/', function (req, res, next) {
     });
   };
 
-
-  let filterValues = {};
-  filterValues['code'] = ["cd - 1000", "cd - 1010", "cd - 1020", "cd - 1030"];
-  filterValues['address'] = ["Address 1", "Address 2"];
-
   let customers = [];
-  let count = 1000;
-  for (let i = 0; i < top; i++) {
-
-    let nextValue = {
-      id: i + skip,
-      name: "Customer " + i + skip,
-      code: "cd - " + (100 + i + skip),
-      address: "Bolivia, Cochabamba, America St " + i + skip,
-      budget: 10 * i
-    };
+  let i = 0;
+  let limitSearch = allData.length;
+  while ( i < limitSearch) {
+    let nextValue = allData[i];
     if (containsIn(nextValue, filterArr)) {
       customers.push(nextValue);
+    }
+    i++;
+  }
+
+  if (req.query.$orderby) {
+    let sortFiltersParam = req.query.$orderby;
+    sortFiltersParam = decodeURIComponent(sortFiltersParam);
+
+    sortFiltersParam = sortFiltersParam.split("AND");
+
+    let keyValSort = {};
+
+    sortFiltersParam.forEach(x => {
+      let keyVal = x.trim().split(" ");
+      keyValSort[keyVal[0]] = keyVal[1];
+    });
+
+    customers.sort((x, y) => {
+      let res = 0;
+      for(var k in keyValSort) {
+        res = x[k].localeCompare(y[k]);
+        if (res != 0) {
+          break;
+        }
+      }
+      return res;
+    });
+    if (req.query.$orderby.includes("desc")) {
+      customers.reverse();
     }
   }
 
   res.send({
-    count: count,
-    value: customers,
+    count: customers.length,
+    value: customers.slice(skip, skip + top),
     filterValues: filterValues
   });
 });
 
 function containsIn(el, fieldValues) {
-  console.log(el);
-  console.log(fieldValues);
   for (var k in el) {
-    console.log(k);
     if (fieldValues[k]) {
-      console.log(fieldValues[k]);
       let filtered = fieldValues[k].find(x => el[k] == x);
-      console.log(filtered + "FILTERED");
       if (!filtered) {
         return false;
       }
@@ -91,13 +125,35 @@ router.get('/data/:id', function (req, res, next) {
 });
 
 router.post('/data', function (req, res, next) {
-  console.log(req.body);
-  res.send(req.body);
+  let nextId = allData.length;
+  let nextValue = req.body;
+  nextValue.id = nextId;
+  allData.push(nextValue);
+  insertFilter(nextValue.name, filterValues['name']);
+  insertFilter(nextValue.code, filterValues['code']);
+  insertFilter(nextValue.address, filterValues['address']);
+  res.send(nextValue);
 });
 
+function insertFilter(fil, filters){
+  let found = filters.find(x => x == fil);
+  if (!found) {
+    filters.push(fil);
+  }
+}
 router.put('/data/:id', function (req, res, next) {
-  console.log(req.body);
-  res.send(req.body);
+  let found = allData.find(x => x.id = req.body.id);
+  found.name = req.body.name;
+  found.code = req.body.code;
+  found.name = req.body.name;
+  found.address = req.body.address;
+
+  insertFilter(found.name, filterValues['name']);
+  insertFilter(found.code, filterValues['code']);
+  insertFilter(found.address, filterValues['address']);
+  
+
+  res.send(found);
 });
 
 module.exports = router;
